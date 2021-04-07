4579e54533641c9b12b3020ee86e82e744978943
Zachary Wang <zlwang@haunter.cs.uchicago.edu>
2021-04-07 01:27:23 -0500

    Progress

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -3,31 +3,40 @@ import rospy
 from geometry_msgs.msg import Twist, Vector3
 import math
 
+turning_twist = Twist(linear = Vector3(), angular = Vector3(0, 0, math.pi))
+stopped = Twist(linear = Vector3(), angular = Vector3()) 
+forward_twist = Twist(linear = Vector3(1, 0, 0), angular = Vector3())
+
 class DriveInSquare(object):
     def __init__(self):
         rospy.init_node('Drive_in_circle')
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.twist = Twist(linear = Vector3(), angular = Vector3()) 
+        self.twist = stopped
         #default, blank twist to start
 
     def go_forward(self):
-        self.twist.linear.x = 1
-        self.twist.angular.z = 0
+        self.twist = forward_twist
         self.pub.publish(self.twist)
         rospy.sleep(3)
+        self.twist = stopped
+        self.pub.publish(self.twist)
 
     def turn(self):
-        self.twist.linear.x = 0
-        self.twist.angular.z = 2 * math.pi
+        self.twist = turning_twist
         self.pub.publish(self.twist)
-        rospy.sleep(.25)
+        rospy.sleep(1)
+        self.twist = stopped
+        self.pub.publish(self.twist)
 
 
     def run(self):
-        for(i in range(0, 4)):
-            self.go_forward(self)
-            self.turn(self)
+        for i in range(0, 4) :
+            self.go_forward()
+            rospy.sleep(5)
+            self.turn()
+            rospy.sleep(5)
+
 
 if __name__ == '__main__':
-    node = StopAtWall()
+    node = DriveInSquare()
     node.run()
