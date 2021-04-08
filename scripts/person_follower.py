6abe2afdde7b5b10a29c73a5772eb1e267763731
Zachary Wang <zlwang@haunter.cs.uchicago.edu>
2021-04-08 16:41:53 -0500

    Edited, person done

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -17,24 +17,28 @@ class Follower:
                 #want to find the minimum of all elements in scan
                 #this should tell us which way the object is
                 temp = min(msg.ranges)
+                #stop at 1 unit away. If too close back up
                 dist_err = temp - 1
+                #index tells us rough angle, 0 is in front
+                # index increases corresponds to clockwise around turtle
                 min_Scanned_Ind = msg.ranges.index(temp)
-                angle_err = min_Scanned_Ind * 0.01
+                #angle_err = min_Scanned_Ind * 0.01
                 if min_Scanned_Ind in list(range(10)) + list(range(349, 359)):
                     k_turn = 0
-                elif min_Scanned_Ind in range(11, 180):
-                    k_turn = 0.2
+                elif min_Scanned_Ind in range(11, 179):
+                    k_turn = 0.3
                 else:
-                    k_turn = -0.2
-                k_forward = 0.5
-                if math.isinf(temp):
-                    pass
-                else: 
+                    k_turn = -0.3
+                k_forward = 0.15
+                if math.isinf(temp): #no object exists, dont move
+                    self.pub.publish(Twist())
+                else: #object does exist, drive and turn towards
                     self.twist.linear.x = dist_err * k_forward
                     self.twist.angular.z = k_turn
                     self.pub.publish(self.twist)
 
         def run(self):
+            #keep the scan running
                 rospy.spin()
                 
 if __name__ == '__main__':
